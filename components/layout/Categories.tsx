45bfa35582efb9d8f369a572f9c33488bba9920f
moashraf86 <botoashraf123@gmail.com>
2025-07-06 19:33:25 +0300

    refactor(page): simplify Home component by removing loader logic and converting to async function; add "use client" directive to Categories and WhyChooseUs components

--- components/layout/Categories.tsx
+++ components/layout/Categories.tsx
@@ -1,3 +1,4 @@
+"use client";
 import { useIntersectionObserver } from "@uidotdev/usehooks";
 import Image from "next/image";
 import Link from "next/link";
--- components/shared/WhyChooseUs.tsx
+++ components/shared/WhyChooseUs.tsx
@@ -1,3 +1,4 @@
+"use client";
 import Image from "next/image";
 import { useRef, useState, useEffect } from "react";
 import { useIntersectionObserver } from "@uidotdev/usehooks";
